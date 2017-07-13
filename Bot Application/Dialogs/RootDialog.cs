482cd3fbc1a465032aec3cd8e0c5e0e415cbcc9d
Chaudhary <manochaudhary@deloitte.com>
2017-07-13 17:37:55 +0530

    bot application

--- Bot Application/Dialogs/RootDialog.cs
+++ Bot Application/Dialogs/RootDialog.cs
@@ -2,6 +2,8 @@
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
+using Bot_Application.Services;
+using Bot_Application.Serialization;
 
 namespace Bot_Application.Dialogs
 {
@@ -15,7 +17,7 @@ namespace Bot_Application.Dialogs
             return Task.CompletedTask;
         }
 
-        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        private async Task MessageReceivedAsync1(IDialogContext context, IAwaitable<object> result)
         {
             var activity = await result as Activity;
 
@@ -26,6 +28,82 @@ namespace Bot_Application.Dialogs
             await context.PostAsync($"You sent {activity.Text} which was {length} characters");
 
             context.Wait(MessageReceivedAsync);
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;
+        //    private static async void Response(IDialogContext context, Activity message)
+        //{
+          //  Activity activity = new Activity();
+            var response = await Luis.GetResponse(activity.Text);
+
+            if (response != null)
+            {
+                var intent = new Intent();
+                var entity = new Entity1();
+
+                string productType = string.Empty;
+                string product = string.Empty;
+                string available = string.Empty;
+                string agendaResult = string.Empty;
+
+                foreach (var item in response.entities)
+                {
+                    switch (item.type)
+                    {
+                        case "phone":
+							product = item.entity;
+							productType = item.type;
+							available = item.entity;
+							break;
+                        case "camera":
+                            available = item.entity;
+							product = item.entity;
+							productType = item.type;
+							break;
+						case "books":
+							product = item.entity;
+							available = item.entity;
+							productType = item.type;
+							break;
+						case "electronics":
+							available = item.entity;
+							product = item.entity;
+							productType = item.type;
+							break;
+						case "emotion::happiness":
+							available = item.entity;
+							product = item.entity;
+							productType = item.type;
+							break;
+						case "emotion::sadness":
+							available = item.entity;
+							product = item.entity;
+							productType = item.type;
+							break;
+						case "emotion::fear":
+							available = item.entity;
+							product = item.entity;
+							productType = item.type;
+							break;
+					}
+                }
+
+                if (!string.IsNullOrEmpty(product))
+                {
+                    if (!string.IsNullOrEmpty(available))
+                        // return our reply to the user
+                        await context.PostAsync($"Hello, {product} available in category type {productType}");
+                   // resposta = message.CreateReplyMessage($"OK! Entendi, mostrando {agendaInf} de {pessoa}");
+                    else
+                        await context.PostAsync($"Hello, Its not available.");
+                    //resposta = message.CreateReplyMessage("Not available for " + pessoa + ".");
+                }
+                else
+                    await context.PostAsync($"No result found");
+            }
+            
         }
     }
 }
---
+++ Bot Application/Services/Luis.cs
@@ -0,0 +1,36 @@
+﻿using Bot_Application.Serialization;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bot_Application.Services
+{
+    public class Luis
+    {
+        public static async Task<Utterance> GetResponse(string message)
+        {
+            using (var client = new HttpClient())
+            {
+                const string authKey = "cb7515b8a93749b7bb11f550c899345b";
+
+                 var url = $"https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/eed7d7f3-dd95-4cf3-beea-d3c9731bf25e?subscription-key={authKey}&timezoneOffset=0&verbose=true&q={message}";
+				client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = await client.GetAsync(url);
+
+                if (!response.IsSuccessStatusCode) return null;
+                var result = await response.Content.ReadAsStringAsync();
+
+                var js = new DataContractJsonSerializer(typeof(Utterance));
+                var ms = new MemoryStream(Encoding.ASCII.GetBytes(result));
+                var list = (Utterance)js.ReadObject(ms);
+
+                return list;
+            }
+        }
+    }
+}
