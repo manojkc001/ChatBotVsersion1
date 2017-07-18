0b22debc5ec8a35538757c9034885ff64d99c7d0
Chaudhary <manochaudhary@deloitte.com>
2017-07-18 13:45:14 +0530

    Configuration added

---
+++ Bot Application/Configuration/SensitiveDataConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.Serialization;
+
+namespace Bot_Application
+{
+    [Serializable()]
+    public class SensitiveDataConfig
+    {
+        [XmlElement("Node1")]
+        public string Node1 { get; set; }
+
+        [XmlElement("Node2")]
+        public string Node2 { get; set; }
+
+        [XmlElement("Node3")]
+        public string Node3 { get; set; }
+
+        [XmlElement("Node4")]
+        public string Node4 { get; set; }
+
+        [XmlElement("Node5")]
+        public string Node5 { get; set; }
+
+    }
+}
---
+++ Bot Application/Configuration/SensitiveDataConfigCollection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.Serialization;
+
+namespace Bot_Application
+{
+    [Serializable]
+    [XmlRoot("UserConfig")]
+    public class SensitiveDataConfigCollection
+    {
+        [XmlArray("Users")]
+        [XmlArrayItem("User",typeof(SensitiveDataConfig))]
+        public List<SensitiveDataConfig> Items { get; set; }
+    }
+}
