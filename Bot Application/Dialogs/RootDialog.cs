783295e7994bf3211439039d0becc2e3f8126628
Atreya Bvs <ratreyabvs@deloitte.com>
2017-07-27 16:15:23 +0530

    Commitng with new changes

--- Bot Application/Dialogs/RootDialog.cs
+++ Bot Application/Dialogs/RootDialog.cs
@@ -39,7 +39,7 @@ namespace Bot_Application.Dialogs
 
             var activity = await result as Activity;
 
-            ReturnAvailableCards("CreditCard");
+            
 
             //    private static async void Response(IDialogContext context, Activity message)
             //{
@@ -48,59 +48,122 @@ namespace Bot_Application.Dialogs
 
             if (response != null)
             {
-                var intent = new Intent();
-                var entity = new Entity1();
-
-                string productType = string.Empty;
-                string product = string.Empty;
-                string available = string.Empty;
-                string agendaResult = string.Empty;
+                List<string> entities = new List<string>();
 
                 foreach (var item in response.entities)
                 {
-                    switch (item.type)
-                    {
-                        
-                        case "electronics":
-                            available = item.entity;
-                            product = item.entity;
-                            productType = item.type;
-                            break;
-                        case "classic features":
-                            productType = item.type;
-                            ReturnMatch("Classic");
-                            break;
-
-                        case "CCInformation":
-                            available = "CreditCard";
-                            List<string> CardsOffered = new List<string>();
-                            CardsOffered = ReturnAvailableCards(available);
-                            string combindedString = string.Join(",", CardsOffered.ToArray());
-
-                            //ReturnAvailbleCards()
-                            break;
-                    }
+                    entities.Add(item.type);
                 }
-
-                if (!string.IsNullOrEmpty(product))
+                string combindedentity = string.Join(",", entities);
+                if (combindedentity.Contains("CCInformation") || combindedentity.Contains("CreditCard"))
                 {
-                    if (!string.IsNullOrEmpty(available))
-                        // return our reply to the user
-                        await context.PostAsync($"Hello, {product} available in category type {productType}");
-                    // resposta = message.CreateReplyMessage($"OK! Entendi, mostrando {agendaInf} de {pessoa}");
-                    else
-                        await context.PostAsync($"Hello, Its not available.");
-                    //resposta = message.CreateReplyMessage("Not available for " + pessoa + ".");
+                    var Creditcardsinfo = ReturnAvailableCards("CreditCard");
+                    await context.PostAsync($"Hello,The follwing cards are offered \n\n{Creditcardsinfo} ");
                 }
-                else
-                    await context.PostAsync($"No result found");
+                if (combindedentity.Contains("Classic"))
+                {
+                    var InfoToUser = ReturnInfoForClassic("Classic");
+
+                    if (combindedentity.Contains("rewards"))
+                    {
+                        await context.PostAsync($"Hello,Currently there are no rewards on Classic. Opt Platinum card for rewards.");
+                    }
+                    else if (combindedentity.Contains("eligibility"))
+                    {
+                        await context.PostAsync($"Hello,To be eligible for a Classic Card {InfoToUser.ClassicInformation.Eligibility} ");
+                    }
+                    else if (combindedentity.Contains("credit limit"))
+                    {
+                        await context.PostAsync($"Hello,The credit limit for a Classic Card is {InfoToUser.ClassicInformation.CardLimit} ");
+                    }
+                    else if (combindedentity.Contains("interestrate"))
+                    {
+                        await context.PostAsync($"Hello, {InfoToUser.ClassicInformation.InterestRatesForCards} ");
+                    }
+                    else if (combindedentity.Contains("fee"))
+                    {
+                        await context.PostAsync($"Hello,We are happy to tell you that  Classic card has {InfoToUser.ClassicInformation.AnnualFees} ");
+                    }
+                    else if (combindedentity.Contains("features") || combindedentity.Contains("credit card"))
+                    {
+                        await context.PostAsync($"Hello, Classic card has the following features - \n\nAnnual Fees - {InfoToUser.ClassicInformation.AnnualFees} \n\n CardLimit is {InfoToUser.ClassicInformation.CardLimit} AUD \n\n To be eligible {InfoToUser.ClassicInformation.Eligibility} \n\n {InfoToUser.ClassicInformation.InterestRatesForCards}");
+                    }
+
+                }
+                if (combindedentity.Contains("Platinum"))
+                {
+                    var InfoToUser = ReturnInfoForPlatinum("Platinum");
+                    if (combindedentity.Contains("rewards") || combindedentity.Contains("benefits"))
+                    {
+                        await context.PostAsync($"Hello, You can get upto 1% cashback maximum upto 30 AUD.");
+                    }
+
+                   else if (combindedentity.Contains("features")|| combindedentity.Contains("credit card"))
+                    {
+                        await context.PostAsync($"Hello, Platinum card has the following features - \n\nAnnual Fees - {InfoToUser.PlatinumInformation.AnnualFees} \n\n CardLimit is {InfoToUser.PlatinumInformation.CardLimit} AUD \n\n To be eligible {InfoToUser.PlatinumInformation.Eligibility} \n\n {InfoToUser.PlatinumInformation.InterestRatesForCards}");
+                    }
+                    else if (combindedentity.Contains("eligibility"))
+                    {
+                        await context.PostAsync($"Hello,To be eligible for a Platinum Card {InfoToUser.PlatinumInformation.Eligibility} ");
+                    }
+                    else if (combindedentity.Contains("credit limit"))
+                    {
+                        await context.PostAsync($"Hello,The credit limit for a Platinum Card is {InfoToUser.PlatinumInformation.CardLimit} AUD ");
+                    }
+                    else if (combindedentity.Contains("interestrate"))
+                    {
+                        await context.PostAsync($"Hello, {InfoToUser.PlatinumInformation.InterestRatesForCards} ");
+                    }
+                    else if (combindedentity.Contains("fee"))
+                    {
+                        await context.PostAsync($"Hello,Platinum card has an Annual fee of {InfoToUser.PlatinumInformation.AnnualFees} AUD");
+                    }
+                    
+                }
+
+                //switch (item.type)
+                //{
+
+                //    case "electronics":
+                //        available = item.entity;
+                //        product = item.entity;
+                //        productType = item.type;
+                //        break;
+                //    case "classic features":
+                //        productType = item.type;
+                //        //("Classic");
+                //        break;
+
+                //    case "CCInformation":
+                //        available = "CreditCard";
+                //        List<string> CardsOffered = new List<string>();
+                //        CardsOffered = ReturnAvailableCards(available);
+                //        string combindedString = string.Join(",", CardsOffered.ToArray());
+
+                //        //ReturnAvailbleCards()
+                //        break;
+                //}
+
+
+                //if (!string.IsNullOrEmpty(product))
+                //{
+                //    if (!string.IsNullOrEmpty(available))
+                //        // return our reply to the user
+                //        await context.PostAsync($"Hello, {product} available in category type {productType}");
+                //    // resposta = message.CreateReplyMessage($"OK! Entendi, mostrando {agendaInf} de {pessoa}");
+                //    else
+                //        await context.PostAsync($"Hello, Its not available.");
+                //    //resposta = message.CreateReplyMessage("Not available for " + pessoa + ".");
+                //}
+                //else
+                //    await context.PostAsync($"No result found");
 
 
             }
 
         }
 
-        public List<string> ReturnAvailableCards(string entity)
+        public string ReturnAvailableCards(string entity)
         {
             List<string> AvailableCards = new List<string>();
             var xdoc = XDocument.Load("C:\\Path\\SensitiveDataMapping.xml");
@@ -112,7 +175,7 @@ namespace Bot_Application.Dialogs
                
             }
             string combindedString = string.Join(",", AvailableCards);
-            return AvailableCards;
+            return combindedString;
         }
 
         public Platinum ReturnInfoForPlatinum(string entity, string compositeentity = "")
